7ca8631a443d0176545d51aaa15f6893e23c3552
Steven Gort <steven.gort@ictu.nl>
2023-11-30 10:09:38 +0100

    initial config

--- js/config.js
+++ js/config.js
@@ -1,43 +1,58 @@
 let respecConfig = {
+
+
+  // Config parameters
   useLogo: true,
   useLabel: true,
-  license: "cc-by",
-  specStatus: "DEF",
+  license: "cc0",
+  specStatus: "WV",
   specType: "HR",
-  pubDomain: "dk",
-  shortName: "template",
-  publishDate: "2023-06-21",
-  publishVersion: "0.0.3",
+  pubDomain: "ronl",
+
+  // shortName = Repository name
+  shortName: "wetsanalyse",
+  publishDate: "2023-11-30",
+  publishVersion: "0.0.1",
   
-  previousPublishVersion: "0.0.2",
-  previousPublishDate: "2022-06-24",
+  previousPublishVersion: "",
+  previousPublishDate: "",
   previousMaturity: "WV",
-  title: "titleText",
+  title: "Wetsanalyse",
+  subtitle: "voor een werkbare uitvoering van wetgeving met ICT",
+
+
+  // Content
   content: {"ch01": "informative", "ch02": "", "mermaid": ""},
-  editors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
+  editors: [{
+    name: "Mildo van Staden",
+    mailto: "Mildo.Staden@minbzk.nl",
+    company: "BZK",
+    companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+  }, {
+    name: "Mariette Lokin",
+    mailto: "mariette@hooghiemstra-en-partners.nl",
+    company: "Hooghiemstra & Partners",
+    companyURL: "https://hooghiemstra-en-partners.nl",
+  }],
   authors:
     [
       {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        name: "Mariette Lokin",
+        url: "https://hooghiemstra-en-partners.nl",
+      },
+      {
+        name: "Steven Gort",
+        url: "https://www.ictu.nl",
       }
     ],
-  github: "https://github.com/Logius-standaarden/ReSpec-template",
+  github: "https://github.com/MinBZK/wetsanalyse/",
 
 
   // Create PDF and link to file in header (optional):
   alternateFormats: [
       {
           label: "pdf",
-          uri: "template.pdf",
+          uri: "Wetsanalyse.pdf",
       },
   ],
 };
