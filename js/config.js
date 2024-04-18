5b29027f9506d7a825c0ef41a34fe057ee3065c3
Steven Gort <steven.gort@ictu.nl>
2024-04-18 11:45:28 +0200

    restructure content v6

--- js/config.js
+++ js/config.js
@@ -50,6 +50,7 @@ let respecConfig = {
   localBiblio: {
     HOHFELD: {
       title: "Some Fundamental Conceptions as Applied in Judicial Reasoning",
+      href: "https://www.jstor.org/stable/785533",
       publisher: "Yale Law Journal (23) 1913, afl. 1, p. 16-59",
       },
   },
